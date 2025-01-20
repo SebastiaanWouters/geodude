86efded9e6f1fe82ce234946520f60cbdada9359
Sebastiaan Wouters <sebastiaanwouters@tutanota.com>
2025-01-20 22:32:27 +0100

    chore: initialize go module with osm pbf parser

---
+++ internal/osm/parser.go
@@ -0,0 +1,50 @@
+package osm
+
+import (
+	"context"
+	"os"
+
+	"github.com/paulmach/osm"
+	"github.com/paulmach/osm/osmpbf"
+)
+
+type OSMData struct {
+	Nodes map[osm.NodeID]osm.Node
+	Ways  []osm.Way
+}
+
+func ParsePBF(filePath string) (*OSMData, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := osmpbf.New(context.Background(), file, 3)
+	defer scanner.Close()
+
+	data := &OSMData{
+		Nodes: make(map[osm.NodeID]osm.Node),
+		Ways:  make([]osm.Way, 0),
+	}
+
+	for scanner.Scan() {
+		o := scanner.Object()
+
+		switch v := o.(type) {
+		case *osm.Node:
+			data.Nodes[v.ID] = *v
+		case *osm.Way:
+			if v.Tags.Find("highway") != "" {
+				data.Ways = append(data.Ways, *v)
+			}
+		}
+	}
+
+	scanErr := scanner.Err()
+	if scanErr != nil {
+		panic(scanErr)
+	}
+
+	return data, nil
+}
