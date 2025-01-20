a8c41a44676647fc821fe34990695bce1ff962aa
Sebastiaan Wouters <sebastiaanwouters@tutanota.com>
2025-01-20 22:29:58 +0100

    test(osm): test parsePBF function

---
+++ internal/osm/parser_test.go
@@ -0,0 +1,31 @@
+package osm
+
+import (
+	"testing"
+)
+
+func TestParsePBF(t *testing.T) {
+
+	data, err := ParsePBF("./../../data/andorra-latest.osm.pbf")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(data.Nodes) != 471006 {
+		t.Fatalf("Expected 471006 nodes, got %d", len(data.Nodes))
+	}
+
+	if data.Nodes[3854300084].Lon != 1.5963506 {
+		t.Fatalf("Expected Longitude 1.5963506, got %f", data.Nodes[3854300084].Lon)
+	}
+
+	if data.Nodes[3854300084].Lat != 42.491302600000004 {
+		t.Fatalf("Expected Latitude 42.491302600000004, got %f", data.Nodes[3854300084].Lat)
+	}
+
+	if len(data.Ways) != 7763 {
+		t.Fatalf("Expected 7763 ways, got %d", len(data.Ways))
+	}
+
+}
